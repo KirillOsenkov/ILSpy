cbab32b635e8bd7b033b726d69c3ff1296c7bf53
Kirill Osenkov <github@osenkov.com>
2019-04-19 22:11:48 -0700

    Add another sequence point test.

--- ICSharpCode.Decompiler.Tests/Util/SequencePointTests.cs
+++ ICSharpCode.Decompiler.Tests/Util/SequencePointTests.cs
@@ -15,16 +15,49 @@ namespace ICSharpCode.Decompiler.Tests.Util
 		[Test]
 		public void BasicSequencePoints()
 		{
-			TestCreateSequencePoints(@"class C { void M() { int i = 0; int j = 1; } }",
+			TestCreateSequencePoints(@"class C
+{
+	void M()
+	{
+		int i = 0;
+		int j = 1;
+
+		System.Action a = () => 
+		{
+			int k = 2;
+		};
+	}
+}",
 				"int num = 0;",
 				"int num2 = 1;");
+		}
+
+		private DecompilerSettings decompilerSettings = new DecompilerSettings {
+			ThrowOnAssemblyResolveErrors = false,
+			AsyncAwait = false,
+			AnonymousMethods = false,
+			AnonymousTypes = false,
+			ArrayInitializers = false,
+			AwaitInCatchFinally = false,
+			ShowDebugInfo = true
+		};
+
+		[Test]
+		public void RealDll()
+		{
+			var decompiler = new CSharp.CSharpDecompiler(@"C:\temp\1\src\console\bin\Debug\net472\console.exe", decompilerSettings);
+			TestCreateSequencePoints(decompiler, "console.Program");
 		}
 
 		private void TestCreateSequencePoints(string code, params string[] expectedSequencePoints)
 		{
 			var decompiler = Tester.GetDecompilerForSnippet(code);
+			TestCreateSequencePoints(decompiler, "C", expectedSequencePoints);
+		}
 
-			var tree = decompiler.DecompileType(new FullTypeName("C"));
+		private void TestCreateSequencePoints(CSharp.CSharpDecompiler decompiler, string typeName, params string[] expectedSequencePoints)
+		{
+			var tree = decompiler.DecompileType(new FullTypeName(typeName));
 
 			var output = new StringWriter();
 			tree.AcceptVisitor(new InsertParenthesesVisitor { InsertParenthesesForReadability = true });
@@ -39,7 +72,7 @@ namespace ICSharpCode.Decompiler.Tests.Util
 			var lines = finalText.Split(new[] { output.NewLine }, StringSplitOptions.None);
 
 			var actualSequencePoints = new List<string>();
-			foreach (var sequencePoint in functionsWithSequencePoints.Values.First()) {
+			foreach (var sequencePoint in functionsWithSequencePoints.Values.SelectMany(f => f)) {
 				if (sequencePoint.IsHidden) {
 					continue;
 				}
